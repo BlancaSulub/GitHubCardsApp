c0e6733287927bc583e67c73ed78c129f62ea300
BlancaSulub <17470064@campeche.tecnm.mx>
2021-07-09 18:12:44 -0500

    Lee un nombre de usuario de GitHub y obtiene su perfil a travesdel API de GitHub

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import CardList from './CardList';
-
+import Form from './Form';
 class App extends React.Component {
  constructor(props) {
  super(props);
+ this.state = {
+ profiles: [],
+ };
  }
+ addNewProfile = (profileData) => {
+ console.log(profileData);
+ this.setState(prevState => ({
+ profiles: [...prevState.profiles, profileData]
+ }));
+ };
  
  render() {
  return (
@@ -12,11 +21,14 @@ class App extends React.Component {
  <div className="header">
  {this.props.title}
  </div>
- <CardList />
+ <Form onSubmit={this.addNewProfile} />
+ <CardList profiles={this.state.profiles} />
  </div>
  );
  }
 }
 export default App;
+
+   
 
 
